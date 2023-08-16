ab73ab158ef590e90c3ff2b038d45d22e64f68a9
cc0d3r <120825275+NormalShit@users.noreply.github.com>
2023-08-15 19:26:37 -0500

    no tengo idea si va a funcionar :D

--- src/Potions/PotLoader.php
+++ src/Potions/PotLoader.php
@@ -1,3 +1,23 @@
 <?php
 
 namespace Potions;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Server;
+
+use Potions\entity\listener\Usage;
+use Potions\entity\CustomPot;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents(new Usage(), $this);
+        $this->register();
+    }
+
+    public function register(): void {
+        $entityMap = $this->getServer()->getEntityFactory();
+        $entityMap->register(CustomPot::class, true);
+    }
+}
